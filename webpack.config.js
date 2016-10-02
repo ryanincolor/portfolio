4c182e06e1037411a1c5f4a9550fd9bb3f6576a3
ryanincolor <ryan@bravelikeus.com>
2016-10-02 19:55:41 +0200

    Add basic webpack config

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const validate = require('webpack-validator');
+const merge = require('webpack-merge');
+const parts = require('./libs/parts');
+
+
+
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
+};
+
+const common = {
+  entry: {
+    app: PATHS.app
+  },
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Portfolio of Ryan Pittman'
+    })
+  ],
+};
+
+var config; 
+
+switch(process.env.npm_lifecycle_event) {
+  case 'build':
+    config = merge(
+      common, 
+
+      {
+        devtool: 'source-map'
+      }, 
+      parts.setupCSS(PATHS.app)
+    );
+  default:
+    config = merge(
+      common, 
+
+      { 
+        devtool: 'eval-source-map'
+      },
+      parts.setupCSS(PATHS.app), 
+      parts.devServer({
+        host: process.env.HOST,
+        port: 8000
+      })
+    );
+}
+
+module.exports = validate(config);
