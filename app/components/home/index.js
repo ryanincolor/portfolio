2ed26826135e216e78ac6dd47a12a1980c7417ad
ryanincolor <ryan@bravelikeus.com>
2016-10-04 13:47:54 +0200

    Add dribble api

--- app/components/home/index.js
+++ app/components/home/index.js
@@ -1,28 +1,49 @@
 import React from "react";
 import styles from './styles.css';
 
-import data from '../../dribbble.json';
+window.jQuery = require('jquery');
+var jribbble = require('jribbble');
+
+
 
 class Home extends React.Component {
   constructor(props) {
     super(props); 
     this.state = {
-      shots : data
+      shots : false
     }
   }
 
+  componentDidMount() {
+    jQuery.jribbble.setToken('7875d1a58ad42ec2f9765322b6fe9557eea26f7694a7a0621a7980d9ac90f7ef');
+    jQuery.jribbble.users('ryanincolor').shots().then(function(res) {
+      this.setState({
+        shots: res
+      });
+    }.bind(this));
+  }
+
   render() {
+    if( !this.state.shots ) {
+      return (
+        <div className={styles.home}>
+          <ul className={styles.shotlist}>
+            <h3>Loading...</h3>
+          </ul>
+        </div>
+      );
+    } 
+    
     var shotList = this.state.shots.map(function(shot){
       return(
-        <li key={shot.id}><img src={shot.images.teaser} /></li>
+        <li key={shot.id}><img src={shot.images.hidpi} /></li>
       )
     });
-
-    return (
+    return (  
       <div className={styles.home}>
-      <ul className={styles.shotlist}>
-        {shotList}
-      </ul>
+        <ul className={styles.shotlist}>
+          {shotList}
+        </ul>
       </div>
     )
   }
