6d8a2864bacc1f3c1795702ec75557dc1b769785
ryanincolor <ryan@bravelikeus.com>
2016-10-04 12:53:16 +0200

    Add dribble posts

--- app/components/home/index.js
+++ app/components/home/index.js
@@ -1,17 +1,28 @@
 import React from "react";
 import styles from './styles.css';
 
+import data from 'json!../../dribbble.json';
+
 class Home extends React.Component {
   constructor(props) {
-    super(props);
+    super(props); 
     this.state = {
+      shots : data
     }
   }
 
   render() {
+    var shotList = this.state.shots.map(function(shot){
+      return(
+        <li key={shot.id}><img src={shot.images.teaser} /></li>
+      )
+    });
+
     return (
-      <div className="home">
-      Sup
+      <div className={styles.home}>
+      <ul className={styles.shotlist}>
+        {shotList}
+      </ul>
       </div>
     )
   }
---
+++ app/components/work/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Work extends React.Component {
+  render() {
+    return ( 
+      <section>Work Goes Here</section>
+    )
+  }
+}
+
+export default Work
